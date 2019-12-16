62ce11ca663d52fcf1062d1e05d097be2347d698
kulichkov <mikhail.kulichkov@gmail.com>
2019-12-16 05:26:05 +0300

    Artwork added

---
+++ Sources/App/Models/Artwork.swift
@@ -0,0 +1,58 @@
+//
+//  Artwork.swift
+//  App
+//
+//  Created by Mikhail Kulichkov on 16.12.2019.
+//
+
+import FluentSQLite
+import Vapor
+
+struct Location: SQLiteModel {
+    var id: Int?
+    var lat: Double?
+    var lng: Double?
+}
+
+struct Artist: SQLiteModel {
+    var id: Int?
+    var image: String?
+    var name: String?
+    var information: String?
+}
+
+/// A single entry of a Artworks.
+final class Artwork: SQLiteModel {
+    
+    var id: Int?
+    var title: String?
+    var location: Location?
+    var icon: String?
+    var excerpt: String?
+    var thumbnailImage: String?
+    var content: String?
+    var image: String?
+    var artist: Artist?
+
+    /// Creates a new `Artwork`.
+    init(id: Int? = nil, title: String? = nil, location: Location? = nil, icon: String? = nil, excerpt: String? = nil, thumbnailImage: String? = nil, content: String? = nil, image: String? = nil, artist: Artist? = nil) {
+        self.id = id
+        self.title = title
+        self.location = location
+        self.icon = icon
+        self.excerpt = excerpt
+        self.thumbnailImage = thumbnailImage
+        self.content = content
+        self.image = image
+        self.artist = artist
+    }
+}
+
+/// Allows `Artwork` to be used as a dynamic migration.
+extension Artwork: Migration { }
+
+/// Allows `Artwork` to be encoded to and decoded from HTTP messages.
+extension Artwork: Content { }
+
+/// Allows `Artwork` to be used as a dynamic parameter in route definitions.
+extension Artwork: Parameter { }
