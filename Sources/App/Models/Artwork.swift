b0b7c20a9f8378ba8a75e3e8ee4ff55d7da4dad2
kulichkov <mikhail.kulichkov@gmail.com>
2019-12-17 17:13:43 +0300

    Artwork init modified

--- Sources/App/Models/Artwork.swift
+++ Sources/App/Models/Artwork.swift
@@ -9,47 +9,49 @@ import FluentSQLite
 import Vapor
 
 struct Location: SQLiteModel {
-    var id: Int?
-    var lat: Double?
-    var lng: Double?
+	var id: Int?
+	var lat: Double?
+	var lng: Double?
 }
 
 struct Artist: SQLiteModel {
-    var id: Int?
-    var image: String?
-    var name: String?
-    var information: String?
+	var id: Int?
+	var image: String?
+	var name: String?
+	var information: String?
 }
 
 /// A single entry of a Artworks.
 final class Artwork: SQLiteModel {
-    
-    var id: Int?
-	var language: String?
-    var title: String?
-    var location: Location?
-    var icon: String?
-	var iconDark: String?
-    var excerpt: String?
-    var thumbnailImage: String?
-    var content: String?
-    var image: String?
-    var artist: Artist?
 
-    /// Creates a new `Artwork`.
-	init(id: Int? = nil, language: String?, title: String? = nil, location: Location? = nil, icon: String? = nil, iconDark: String? = nil, excerpt: String? = nil, thumbnailImage: String? = nil, content: String? = nil, image: String? = nil, artist: Artist? = nil) {
-        self.id = id
+	var id: Int?
+	var language: String?
+	var title: String?
+	var location: Location?
+	var viewPoint: Location?
+	var icon: String?
+	var iconDark: String?
+	var excerpt: String?
+	var thumbnailImage: String?
+	var content: String?
+	var image: String?
+	var artist: Artist?
+
+	/// Creates a new `Artwork`.
+	init(id: Int? = nil, language: String?, title: String? = nil, location: Location? = nil, viewPoint: Location? = nil, icon: String? = nil, iconDark: String? = nil, excerpt: String? = nil, thumbnailImage: String? = nil, content: String? = nil, image: String? = nil, artist: Artist? = nil) {
+		self.id = id
 		self.language = language
-        self.title = title
-        self.location = location
-        self.icon = icon
+		self.title = title
+		self.location = location
+		self.viewPoint = viewPoint
+		self.icon = icon
 		self.iconDark = iconDark
-        self.excerpt = excerpt
-        self.thumbnailImage = thumbnailImage
-        self.content = content
-        self.image = image
-        self.artist = artist
-    }
+		self.excerpt = excerpt
+		self.thumbnailImage = thumbnailImage
+		self.content = content
+		self.image = image
+		self.artist = artist
+	}
 }
 
 /// Allows `Artwork` to be used as a dynamic migration.
--- Sources/App/Models/Artworks.swift
+++ Sources/App/Models/Artworks.swift
@@ -40,27 +40,27 @@ extension Artworks {
 				name: "Alicia Eggert",
 				information: "American interdisciplinary artist Alicia Eggert (1981) lives and works in Texas. She graduated as a sculptor from Alfred University in New York and explores the relationship between language, image and time. Words and statements are used in her installations as found objects, and she often writes them out with (neon) light. Eggert also works as a lecturer and coordinator of the sculpture department at the University of North Texas in Denton.")),
 		Artwork(
-		id: 246,
-		language: "nl",
-		title: "All the light you see",
-		location: Location(
 			id: 246,
-			lat: 52.373759713654003,
-			lng: 4.9141258533172998),
-		viewPoint: Location(
-			id: 246,
-			lat: 52.373485000000002,
-			lng: 4.9126719999999997),
-		icon: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-light-you-see.png",
-		iconDark: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/ALF-icons-kunstwerken-20.png",
-		excerpt: "Licht doet er altijd even over om van het ene punt naar het andere te reizen – om precies te zijn, één seconde per 300.000 kilometer – en dus om onze ogen te bereiken. Die reistijd varieert van bijvoorbeeld 8 minuten voor het licht dat van de zon naar de aarde wordt verzonden, tot miljoenen jaren bij de straling van een ster die zich aan de rand van ons universum bevindt. De informatie die het licht ons brengt, bijvoorbeeld hoe lang geleden die verre ster is ontstaan, is dus eigenlijk altijd oud.",
-		thumbnailImage: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-Light-you-See.",
-		content: "Precies over dat feit gaat het kunstwerk All the Light You See van Alicia Eggert. Zoals in veel van haar andere werk gebruikt Eggert een poëtisch statement, uitgeschreven in licht, dat van betekenis verandert door een kleine ingreep. Doordat een deel van haar tekst ‘All the Light You See is From the Past’ af en toe uitgaat, maakt ze de boodschap nog simpeler: ‘All You See is Past’ – álles wat je ziet ligt tenslotte al achter ons. All the Light You See is daarmee een memento mori (Latijns voor ‘gedenk te sterven’), een kunstwerk dat ons eraan herinnert dat óók wij over niet al te lange tijd tot het verleden zullen behoren.\n\nNet als een neonreclame (vooral populair tussen 1920 en 1960, denk aan Las Vegas) trekt dit tekstwerk gelijk je aandacht, maar Eggert gebruikt die niet om je een product of een dienst te verkopen. Ze hoopt je bewust te maken van de enorme hoeveelheid licht die jou voortdurend vanuit allerlei hoeken bereikt, en je overlaadt met informatie.",
-		image: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-Light-you-See-header.jpg",
-		artist: Artist(
-			id: 246,
-			image: "https://amsterdamlightfestival.com/storage/artists/September2019/Ad6OajkOysbAbN76KPJL.jpg",
-			name: "Alicia Eggert",
-			information: "De Amerikaanse Alicia Eggert (1981) is een interdisciplinaire kunstenaar die woont en werkt in Texas. Ze is afgestudeerd als beeldhouwer aan de Alfred University in New York en onderzoekt in haar werk altijd de relatie tussen taal, beeld en tijd. Woorden en stellingen gebruikt ze in haar installaties als gevonden objecten, en schrijft deze vaak uit met lampjes of neonlicht. Ook werkt Eggert als universitair docent en coördinator van de afdeling beeldhouwkunst aan de University of North Texas in Denton."))
+			language: "nl",
+			title: "All the light you see",
+			location: Location(
+				id: 246,
+				lat: 52.373759713654003,
+				lng: 4.9141258533172998),
+			viewPoint: Location(
+				id: 246,
+				lat: 52.373485000000002,
+				lng: 4.9126719999999997),
+			icon: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-light-you-see.png",
+			iconDark: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/ALF-icons-kunstwerken-20.png",
+			excerpt: "Licht doet er altijd even over om van het ene punt naar het andere te reizen – om precies te zijn, één seconde per 300.000 kilometer – en dus om onze ogen te bereiken. Die reistijd varieert van bijvoorbeeld 8 minuten voor het licht dat van de zon naar de aarde wordt verzonden, tot miljoenen jaren bij de straling van een ster die zich aan de rand van ons universum bevindt. De informatie die het licht ons brengt, bijvoorbeeld hoe lang geleden die verre ster is ontstaan, is dus eigenlijk altijd oud.",
+			thumbnailImage: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-Light-you-See.",
+			content: "Precies over dat feit gaat het kunstwerk All the Light You See van Alicia Eggert. Zoals in veel van haar andere werk gebruikt Eggert een poëtisch statement, uitgeschreven in licht, dat van betekenis verandert door een kleine ingreep. Doordat een deel van haar tekst ‘All the Light You See is From the Past’ af en toe uitgaat, maakt ze de boodschap nog simpeler: ‘All You See is Past’ – álles wat je ziet ligt tenslotte al achter ons. All the Light You See is daarmee een memento mori (Latijns voor ‘gedenk te sterven’), een kunstwerk dat ons eraan herinnert dat óók wij over niet al te lange tijd tot het verleden zullen behoren.\n\nNet als een neonreclame (vooral populair tussen 1920 en 1960, denk aan Las Vegas) trekt dit tekstwerk gelijk je aandacht, maar Eggert gebruikt die niet om je een product of een dienst te verkopen. Ze hoopt je bewust te maken van de enorme hoeveelheid licht die jou voortdurend vanuit allerlei hoeken bereikt, en je overlaadt met informatie.",
+			image: "https://www.amsterdamlightfestival.com/storage/artworks/November2019/All-the-Light-you-See-header.jpg",
+			artist: Artist(
+				id: 246,
+				image: "https://amsterdamlightfestival.com/storage/artists/September2019/Ad6OajkOysbAbN76KPJL.jpg",
+				name: "Alicia Eggert",
+				information: "De Amerikaanse Alicia Eggert (1981) is een interdisciplinaire kunstenaar die woont en werkt in Texas. Ze is afgestudeerd als beeldhouwer aan de Alfred University in New York en onderzoekt in haar werk altijd de relatie tussen taal, beeld en tijd. Woorden en stellingen gebruikt ze in haar installaties als gevonden objecten, en schrijft deze vaak uit met lampjes of neonlicht. Ook werkt Eggert als universitair docent en coördinator van de afdeling beeldhouwkunst aan de University of North Texas in Denton."))
 	]
 }
