701a25b53aefbe006ba2cb3290eb119aef5c9c58
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2019-12-17 16:33:26 +0300

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "It works! Version 0.02"
     }
 
 	router.get("artworks", String.parameter) { req -> String in
